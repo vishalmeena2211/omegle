dc4999bb2fb743c23d666f3cc6f86315e0a7676e
vishalmeena2211 <meenavishal2211@gmail.com>
2024-06-10 22:22:58 +0530

    tsc htaya

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { Socket, io } from "socket.io-client";
 
-const URL = "http://localhost:3000";
+const URL = "https://befitting-prickly-stealer.glitch.me/";
 
 export const Room = ({
     name,
